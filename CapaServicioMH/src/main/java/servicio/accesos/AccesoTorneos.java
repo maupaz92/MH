63950395cd8f58cf95879a61befff618fca3eefb
maupaz92 <maupaz92@hotmail.com>
2014-06-01 09:23:19 -0600

    agregafa la dependencia para exportar los modelos de entidades a formato JSON, utilizando el paquete jackson de resteasy. El acceso a las entidades torneos ya esta mas acorde a lo planeado

--- CapaServicioMH/src/main/java/servicio/accesos/AccesoTorneos.java
+++ CapaServicioMH/src/main/java/servicio/accesos/AccesoTorneos.java
@@ -9,25 +9,30 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
 
+
+
+
+
+import modelos.recursos.ConjuntoDeTorneosModelo;
 import modelos.recursos.TorneoModelo;
 
+
+/**
+ * clase encargada de recibir las solicitudes http para
+ * la direccion relativa "/torneos". Maneja el acceso de los recursos
+ * relacionados con entidades torneo del sistema.
+ * @author maupaz92
+ *
+ */
 @Path("/torneos")
 public class AccesoTorneos {
 
 	public AccesoTorneos()
 	{
 		
-	}
-	
-	@GET	
-	@Produces(MediaType.TEXT_HTML)
-	public String holaMundi()
-	{
-		return "holaMau";
-	}
-	
+	}	
+		
 	
 	@GET
 	@Path("/{nombre}")
@@ -37,12 +42,31 @@ public class AccesoTorneos {
 		return null;
 	}
 	
-	@GET
-	@Path("/torneos")
-	@Produces(MediaType.APPLICATION_JSON)
-	public void getTorneosPorTipo(@PathParam("tipo") String tipoTorneo)
+	/**
+	 * metodo que retorna los torneos registrados en el sistema a traves
+	 * del metodo HTTP GET.
+	 * Envia el recurso por medio de representaciones en xml o json.
+	 * @return
+	 */
+	@GET	
+	@Produces({"application/xml", "application/json"})
+	public ConjuntoDeTorneosModelo getTorneosRegistrados()
 	{
+		TorneoModelo t1 = new TorneoModelo();
+		t1.setNombre("Brasil 2014");
+		t1.setSede("Brasil");
+		t1.setCopa(true);
+		t1.setTipo("selecciones");
 		
+		TorneoModelo t2 = new TorneoModelo();		
+		t2.setNombre("Russia 2018");
+		
+		ConjuntoDeTorneosModelo lista = new ConjuntoDeTorneosModelo();
+		lista.agregarTorneo(t1);
+		lista.agregarTorneo(t2);
+		
+		//return Response.ok(lista).build();
+		return lista;
 	}
 	
 	
@@ -51,6 +75,7 @@ public class AccesoTorneos {
 	public void registrarNuevoTorneo(TorneoModelo torneo)
 	{		
 		System.out.println(torneo.getNombre());
+		//throw new WebApplicationException(Response.Status.NOT_FOUND);
 	}
 	
 	
