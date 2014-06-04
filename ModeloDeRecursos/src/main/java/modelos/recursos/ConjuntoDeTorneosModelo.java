7d0de328d70f8c332e33aee67428f0c8727e6461
maupaz92 <maupaz92@hotmail.com>
2014-06-04 15:20:40 -0600

    cambiado el ConjuntoTorneos la anotacion "torneo" de la lista de torneos, por "records" para trabajar con javascript y json

--- ModeloDeRecursos/src/main/java/modelos/recursos/ConjuntoDeTorneosModelo.java
+++ ModeloDeRecursos/src/main/java/modelos/recursos/ConjuntoDeTorneosModelo.java
@@ -51,7 +51,7 @@ public class ConjuntoDeTorneosModelo {
 	
 	
 	//----getters & setters
-	@XmlElement(name = "torneo")
+	@XmlElement(name = "records")
 	public List<TorneoModelo> getListaTorneos() {
 		return listaTorneos;
 	}
