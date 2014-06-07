84fe9acdf898a635758e080590b89ba837925e90
maupaz92 <maupaz92@hotmail.com>
2014-06-07 14:47:54 -0600

    commit inicial solo para torneos

---
+++ CapaLogicaMH/src/main/java/logica/GestorTorneos.java
@@ -0,0 +1,23 @@
+package logica;
+
+import modelos.recursos.TorneoModelo;
+
+public class GestorTorneos {
+
+	public GestorTorneos()
+	{
+		
+	}
+	
+	/**
+	 * metodo encargado de verificar y registrar un nuevo torneo.
+	 * Se valida que el torneo no haya sido registro previamente.
+	 * @param torneo: torneo a ser registrado
+	 * @return: true si el torneo pudo ser registrado, false en caso contrario
+	 */
+	public boolean registrarNuevoTorneo(TorneoModelo torneo){
+		return true;
+	}
+	
+	
+}
