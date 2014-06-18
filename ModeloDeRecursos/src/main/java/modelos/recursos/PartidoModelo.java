ac00f750ad669d8c134d993b76b84cd17e1e7995
Diego Jiménez Vargas <diejime01@gmail.com>
2014-06-17 23:05:18 -0600

    Cambios realizados: 	Se modificaron ciertos atributos de la clase TorneoModelo 	Se creo el XML de mapeo de dicha clase para hibernate. 	Se agrego la referencia de mapping resource a hibernate.cfg.xml 	Insertar Partido funciona

--- ModeloDeRecursos/src/main/java/modelos/recursos/PartidoModelo.java
+++ ModeloDeRecursos/src/main/java/modelos/recursos/PartidoModelo.java
@@ -8,7 +8,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class PartidoModelo {
 
-	private String torneo;
+	private int id_Partido;
+	private int id_Torneo;
 	private int equipoA;
 	private int equipoB;
 	private int marcadorA;
@@ -19,52 +20,67 @@ public class PartidoModelo {
 	{
 		
 	}
-	@XmlAttribute
-	public String getTorneo() {
-		return torneo;
+	
+	@XmlAttribute	
+	public int getId_Partido() {
+		return id_Partido;
 	}
 
-	public void setTorneo(String torneo) {
-		this.torneo = torneo;
+
+	public void setId_Partido(int id_Partido) {
+		this.id_Partido = id_Partido;
+	}
+
+
+	
+	@XmlAttribute	
+	public int getId_Torneo() {
+		return id_Torneo;
+	}
+	public void setId_Torneo(int id_Torneo) {
+		this.id_Torneo = id_Torneo;
 	}
 
 	@XmlAttribute
 	public int getEquipoA() {
 		return equipoA;
 	}
-
 	public void setEquipoA(int equipoA) {
 		this.equipoA = equipoA;
 	}
+	
 	@XmlAttribute
+
 	public int getEquipoB() {
 		return equipoB;
 	}
-
 	public void setEquipoB(int equipoB) {
 		this.equipoB = equipoB;
 	}
+	
 	@XmlAttribute
 	public int getMarcadorA() {
 		return marcadorA;
 	}
-
 	public void setMarcadorA(int marcadorA) {
 		this.marcadorA = marcadorA;
 	}
+
 	@XmlAttribute
+
 	public int getMarcadorB() {
 		return marcadorB;
 	}
-
 	public void setMarcadorB(int marcadorB) {
 		this.marcadorB = marcadorB;
 	}
+
 	@XmlAttribute
+
+
 	public Date getFecha() {
 		return fecha;
 	}
-
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
