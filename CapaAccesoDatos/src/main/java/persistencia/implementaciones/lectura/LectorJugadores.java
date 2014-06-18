c9f406d61b344b4214ea3632bb8524478679c280
Diego Jiménez Vargas <diejime01@gmail.com>
2014-06-18 04:26:57 -0600

    Se vuelven a Crear los metodos de lectura de jugadores por equipo

--- CapaAccesoDatos/src/main/java/persistencia/implementaciones/lectura/LectorJugadores.java
+++ CapaAccesoDatos/src/main/java/persistencia/implementaciones/lectura/LectorJugadores.java
@@ -33,13 +33,38 @@ public class LectorJugadores implements LectorJugadoresDAO{
 	}
 
 	public List<Object> getJugadoresPorClub(int idEquipo) {
-		// TODO Auto-generated method stub
-		return null;
+		//se obtiene la sesion
+		Session session = HibernateFactory.getSessionFactory().openSession();
+		//se define el query
+		Query query = session.createQuery("from JugadorModelo e where e.equipoActual.id_Equipo = :idEquipo");
+		query.setParameter("idEquipo", idEquipo);			
+		@SuppressWarnings("unchecked")
+		List<Object> list = query.list();//se obtiene la lista del query
+		return list;
 	}
 
-	public List<Object> getJugadoresPorSeleccion(int idPais) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Object> getJugadoresPorSeleccion(int idEquipo) {
+		int Pais = getIdPaisPorEquipo(idEquipo);
+		Session session = HibernateFactory.getSessionFactory().openSession();
+		Query queryJugadores = session.createQuery("from JugadorModelo a where a.pais.id_Pais = :idP");
+		queryJugadores.setInteger("idP", Pais);	
+		@SuppressWarnings("unchecked")
+		List<Object> list = queryJugadores.list();//se obtiene la lista del query*/		
+		return list;
 	}
+	
+	private Integer getIdPaisPorEquipo(int idEquipo)
+	  {
+		  	//se obtiene la sesion
+			Session session = HibernateFactory.getSessionFactory().openSession();
+			//se define el query
+			Query query = session.createQuery("Select pais.id_Pais from EquipoModelo e where e.id_Equipo = :idEquipo");
+			query.setParameter("idEquipo", idEquipo);	
+			//Se obtiene el identificador del Pais al que pertenece el Equipo Consultado. 
+			Integer idPais = (Integer) query.list().get(0);	
+			return idPais;
+
+		  
+	  }
 
 }
